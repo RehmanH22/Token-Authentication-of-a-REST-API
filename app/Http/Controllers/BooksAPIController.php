e5b8a42b36060bb256631281c57e46c3703eb927
Rehman_H <rehmanh289@gmail.com>
2021-05-10 15:40:53 +0100

    Removed edit + create empty function from BooksAPI Controller

--- app/Http/Controllers/BooksAPIController.php
+++ app/Http/Controllers/BooksAPIController.php
@@ -18,16 +18,6 @@ class BooksAPIController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -54,17 +44,6 @@ class BooksAPIController extends Controller
     public function show($id)
     {
         return Book::find($id);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
