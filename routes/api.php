a7664dd396a53d6a41be14e5c6b4f81d271bdab0
Rehman_H <rehmanh289@gmail.com>
2021-05-11 15:20:10 +0100

    Inserted Login/Register API Routes

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,22 @@ use App\Http\Controllers\BooksAPIController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//--------------------------------------------------Public Routes
+//Auth
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+//REST
 Route::get('/books', [BooksAPIController::class, 'index']);
 Route::get('/books/{id}', [BooksAPIController::class, 'show']);
 Route::post('/books', [BooksAPIController::class, 'store']);
 Route::put('/books/{id}', [BooksAPIController::class, 'update']);
 Route::delete('/books/{id}', [BooksAPIController::class, 'destroy']);
 Route::get('/books/search/{name}', [BooksAPIController::class, 'search']);
+
+//--------------------------------------------------Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    //
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
